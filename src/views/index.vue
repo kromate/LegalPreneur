ca1ff4078ab8f9807ba11def3257113278f0347b
kromate <akpananthony33@gmail.com>
2022-09-22 23:49:36 +0100

    cleanup

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,6 +2,7 @@
 	<metainfo>
 		<template v-slot:title="{ content }">{{ content ? `${content}` : `SITE_NAME` }}</template>
 	</metainfo>
+	
 	<default-layout>
 		<nav-bar />
 		<cta />
@@ -16,24 +17,21 @@
 </template>
 
 <script setup lang="ts">
-import defaultLayout from "@/layouts/defaultLayout.vue";
-import NavBar from "@/components/landing/NavBar.vue";
-import cta from "@/components/landing/cta.vue";
-import aboutUs from "@/components/landing/aboutUs.vue";
-import chooseUs from "@/components/landing/chooseUs.vue";
-import values from "@/components/landing/values.vue";
-import partners from "@/components/landing/partners.vue";
-import expertise from "@/components/landing/exp.vue";
-import footerBar from "@/components/landing/footer.vue";
-import modal from "@/components/core/modal.vue";
+import defaultLayout from '@/layouts/defaultLayout.vue'
+import NavBar from '@/components/landing/NavBar.vue'
+import cta from '@/components/landing/cta.vue'
+import aboutUs from '@/components/landing/aboutUs.vue'
+import chooseUs from '@/components/landing/chooseUs.vue'
+import values from '@/components/landing/values.vue'
+import partners from '@/components/landing/partners.vue'
+import expertise from '@/components/landing/exp.vue'
+import footerBar from '@/components/landing/footer.vue'
+import modal from '@/components/core/modal.vue'
 
-import { useMeta } from "vue-meta";
+import { useMeta } from 'vue-meta'
 
 useMeta({
-	title: "Legalpreneur Attorneys & Consulting",
-	htmlAttrs: { lang: "en", amp: true },
-});
+	title: 'Legalpreneur Attorneys & Consulting',
+	htmlAttrs: { lang: 'en', amp: true },
+})
 </script>
-
-<style scoped>
-</style>
